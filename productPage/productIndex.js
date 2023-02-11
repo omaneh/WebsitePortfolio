8fbf5b4354916415635a2d24c5e3ef7d598065cc
omaneh <omanehabso@gmail.com>
2023-02-11 16:47:48 -0500

    add productPage

---
+++ productPage/productIndex.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector(".btn");
+
+function uhOh() {
+return alert("Uh oh. This is not a real link. This is simply a product page based on a FrontendMentor.io challenge. If you would like further information on this challenge, please visit: https://www.frontendmentor.io/challenges/product-preview-card-component-GO7UmttRfa/hub.");
+}
+
+btn.addEventListener("click", uhOh);
